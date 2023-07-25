455439228b889edf0def04708d938182a7aeda4c
Sandeep Sharma <Sandeepsharma2183@gmail.com>
2023-07-25 19:42:43 +0530

    drawer color changes, heading change according to  diff pages

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -2,6 +2,7 @@
 import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import { Menu } from '@mui/icons-material';
 import { styled } from '@mui/material/styles';
+import { useLocation } from 'react-router-dom';
 
 const Header = styled(AppBar)`
   z-index: 1201;
@@ -10,17 +11,31 @@ const Header = styled(AppBar)`
   box-shadow: inset 0 -1px 0 0 #dadce0;
 `
 
+
 const Heading = styled(Typography)`
   color: #5F6368;
   font-size: 24px;
   margin-left: 25px;
 `
 
-
 const HeaderBar = ({ open, handleDrawer }) => {
+  const location = useLocation();
   const logo = 'https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png';
-  const HeadText = 'Keep';
-  
+  const pathname = location.pathname;
+
+
+  let HeadText = 'Keep';
+  if (pathname === '/archive') {
+    HeadText = 'Archive';
+  } else if (pathname === '/note') {
+    HeadText = 'Keep';
+  } else if (pathname === '/delete') {
+    HeadText = 'Delete';
+  }
+
+
+
+
   return (
     <Header open={open}>
       <Toolbar>
@@ -32,10 +47,49 @@ const HeaderBar = ({ open, handleDrawer }) => {
           <Menu />
         </IconButton>
         <img src={logo} alt="logo" style={{width: 30}} />
-        <Heading src={HeadText}/>
+        <Heading>{HeadText}</Heading>
       </Toolbar>
     </Header>
   )
 }
 
 export default HeaderBar;
+
+
+
+
+
+
+
+
+
+
+// const Heading = styled(Typography)`
+//   color: #5F6368;
+//   font-size: 24px;
+//   margin-left: 25px;
+// `
+
+
+// const HeaderBar = ({ open, handleDrawer }) => {
+//   const logo = 'https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png';
+//   const HeadText = "Keep";
+  
+//   return (
+//     <Header open={open}>
+//       <Toolbar>
+//         <IconButton
+//           onClick={() => handleDrawer()}
+//           sx={{ marginRight: '20px'}}
+//           edge="start"
+//         >
+//           <Menu />
+//         </IconButton>
+//         <img src={logo} alt="logo" style={{width: 30}} />
+//         <Heading src={HeadText}/>
+//       </Toolbar>
+//     </Header>
+//   )
+// }
+
+// export default HeaderBar;
