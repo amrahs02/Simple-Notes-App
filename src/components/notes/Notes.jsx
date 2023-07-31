6639d4f4d79a89a0b2930845d077fbce7f19454b
Sandeep Sharma <Sandeepsharma2183@gmail.com>
2023-07-31 20:45:41 +0530

    replace plus icon with extended edit

--- src/components/notes/Notes.jsx
+++ src/components/notes/Notes.jsx
@@ -69,132 +69,13 @@
 
 // // export default Notes;
 
-// import { useContext } from "react";
-// import { Box, Grid, useMediaQuery } from "@mui/material";
-// import { styled } from "@mui/material/styles";
-// import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-// import { DataContext } from "../../context/DataProvider";
-// import Fab from "@mui/material/Fab";
-// import AddIcon from "@mui/icons-material/Add";
-// import { reorder } from "../../utils/common-utils";
-// import Form from "./Form";
-// import Note from "./Note";
-// import EmptyNotes from "./EmptyNotes";
-
-// const DrawerHeader = styled("div")(({ theme }) => ({
-//   ...theme.mixins.toolbar,
-// }));
-
-// const Notes = () => {
-//   const { notes, setNotes } = useContext(DataContext);
-//   const isMobile = useMediaQuery("(max-width: 600px)");
-
-//   const onDragEnd = (result) => {
-//     if (!result.destination) return;
-
-//     const items = reorder(notes, result.source.index, result.destination.index);
-//     setNotes(items);
-//   };
-
-//   return (
-//     <Box sx={{ display: "flex", width: "100%" }}>
-//       <Box sx={{ p: 3, width: "100%" }}>
-//         <DrawerHeader />
-//         <>
-//           {isMobile ? (
-//             <Fab color="primary" aria-label="add">
-//               <AddIcon />
-//             </Fab>
-//           ) : (
-//             <Form />
-//           )}
-//         </>
-//         {notes.length > 0 ? (
-//           <DragDropContext onDragEnd={onDragEnd}>
-//             <Droppable droppableId="droppable">
-//               {(provided, snapshot) => (
-//                 <>
-//                   {isMobile ? (
-//                     // Mobile view
-//                     <Grid
-//                       container
-//                       style={{ marginTop: 16 }}
-//                       {...provided.droppableProps}
-//                       ref={provided.innerRef}
-//                     >
-//                       {notes.map((note, index) => (
-//                         <Draggable
-//                           key={note.id}
-//                           draggableId={note.id}
-//                           index={index}
-//                         >
-//                           {(provided, snapshot) => (
-//                             <Grid
-//                               ref={provided.innerRef}
-//                               {...provided.draggableProps}
-//                               {...provided.dragHandleProps}
-//                               item
-//                             >
-//                               <Note note={note} />
-//                             </Grid>
-//                           )}
-//                         </Draggable>
-//                       ))}
-//                     </Grid>
-//                   ) : (
-//                     // Desktop view
-//                     <Grid
-//                       container
-//                       style={{ marginTop: 16 }}
-//                       spacing={2}
-//                       {...provided.droppableProps}
-//                       ref={provided.innerRef}
-//                     >
-//                       {notes.map((note, index) => (
-//                         <Draggable
-//                           key={note.id}
-//                           draggableId={note.id}
-//                           index={index}
-//                         >
-//                           {(provided, snapshot) => (
-//                             <Grid
-//                               ref={provided.innerRef}
-//                               {...provided.draggableProps}
-//                               {...provided.dragHandleProps}
-//                               item
-//                               xs={12}
-//                               sm={6}
-//                               md={4}
-//                               lg={3}
-//                             >
-//                               <Note note={note} />
-//                             </Grid>
-//                           )}
-//                         </Draggable>
-//                       ))}
-//                     </Grid>
-//                   )}
-//                 </>
-//               )}
-//             </Droppable>
-//           </DragDropContext>
-//         ) : (
-//           <EmptyNotes />
-//         )}
-//       </Box>
-//     </Box>
-//   );
-// };
-
-// export default Notes;
-
 import { useState, useContext } from "react";
 import { Box, Grid, useMediaQuery } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { DataContext } from "../../context/DataProvider";
 import Fab from "@mui/material/Fab";
-import AddIcon from "@mui/icons-material/Add";
+import EditIcon from '@mui/icons-material/Edit';
 import { reorder } from "../../utils/common-utils";
 import Form from "./Form";
 import Note from "./Note";
@@ -235,10 +116,12 @@ const Notes = () => {
               <Fab
                 style={{ position: "absolute", bottom: 16, right: 16 }}
                 color="primary"
-                aria-label="add"
+                variant="extended"
+                aria-label="edit"
                 onClick={handleAddNoteClick}
               >
-                <AddIcon />
+                <EditIcon />
+                Add Note
               </Fab>
             ) : (
           // Desktop view
